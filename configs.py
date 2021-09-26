1306c31ee11c9cbe25ac4d3680477f4b937d721a
Vanajakshi <84600096+munnadaniels@users.noreply.github.com>
2021-09-26 11:20:11 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -20,7 +20,7 @@ class Config(object):
     BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
 
     START_TEXT = """
-Hi Unkil, I am Video Merge Bot!
+Dhandam Ra Dhootha, I am Video Merge Bot!
 I can Merge Multiple Videos in One Video. Video Formats should be same.
 
 Made by Vanajakshi
